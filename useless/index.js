978f401418959b42e81896c4f65818c5650775bb
Naveed GAIE <160273630+NaveedGenAIE@users.noreply.github.com>
2024-10-29 22:02:51 +0500

    This is HTML and CSS projects.

---
+++ useless/index.js
@@ -0,0 +1,10 @@
+// let myArr = [1, 2, 3, 4];
+// let [a, , , d] = myArr;
+// console.log(a);
+// console.log(d);
+// console.log(myArr[0]);
+var myObj = {
+    name1: "Naveed",
+};
+var name1 = myObj.name1;
+console.log(name1);
---
+++ useless/index.ts
@@ -0,0 +1,10 @@
+// let myArr = [1, 2, 3, 4];
+// let [a, , , d] = myArr;
+// console.log(a);
+// console.log(d);
+// console.log(myArr[0]);
+const myObj = {
+  name1: "Naveed",
+};
+const { name1 } = myObj;
+console.log(name1);
